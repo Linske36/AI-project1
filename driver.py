e0cb4d8e5469ac47e42db14d6183ee2794f6cb75
Lindsay Schiminske <ls3245@columbia.edu>
2017-02-12 20:03:01 +0000

    solves for nxn puzzle

--- driver.py
+++ driver.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 import sys
-import queue
+import heapq
 import time
 import resource
+import math
 
 class Node:
     def __init__(self, state, parent, direction, cost, depth):
@@ -17,17 +18,37 @@ class Node:
 def create_Node(state, parent, direction, cost, depth):
     return Node(state, parent, direction, cost, depth)
 
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+        
+    def empty(self):
+        return len(self.elements) == 0
+    
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+
+#def manhattan_Distance(goal_Test):
+    
 
 
 def create_Frontier(node_state):
     state = node_state.state
-#    print("create state", state)
-    top_row = [state[0], state[1], state[2]]
-    mid_row = [state[3], state[4], state[5]]
-    btm_row = [state[6], state[7], state[8]]
+    size = len(state)
+    num = int(math.sqrt(size))
+    board = [state[i:i+num] for i in range(0, size, num)]
+    first_colm = []
+    last_colm = []
+ 
+    for index in range(len(board)): 
+        first_colm.append(board[index][0])
 
-    first_colm = [state[0], state[3], state[6]]
-    last_colm = [state[2], state[5], state[8]]
+    for index in range(len(board)):
+        last_colm.append(board[index][len(board)-1])
 
     pos = state.index('0')
     frontier_Nodes = []
@@ -35,7 +56,7 @@ def create_Frontier(node_state):
 
     
     #Move up
-    if '0' not in top_row:
+    if '0' not in board[0]:
         temp = state[pos - 3]
         new_State[pos - 3] = state[pos]
         new_State[pos] = temp
@@ -45,7 +66,7 @@ def create_Frontier(node_state):
         new_State = list(state)
 
     #Move down
-    if '0' not in btm_row:
+    if '0' not in board[len(board)-1]:
         temp = state[pos + 3]
         new_State[pos + 3] = state[pos]
         new_State[pos] = temp
@@ -83,6 +104,7 @@ def bfs(initial_State, goal_Test):
     start = time.time()
     frontier = []
     explored = set()
+    test_Frontier = set()
     begin = 1
     nodes_expanded = 0
     max_depth = 0
@@ -100,7 +122,8 @@ def bfs(initial_State, goal_Test):
         explored_State = ''.join(map(str, test_State))
         explored.add(explored_State)
 
-
+        if not begin:
+            test_Frontier.remove(explored_State)
 
         if test_State == goal_Test:
             stop = time.time()
@@ -131,15 +154,13 @@ def bfs(initial_State, goal_Test):
    	
         if not frontier and begin == 1:
             for index in range(len(neighbor)):
+                frontier_State = ''.join(map(str, neighbor[index].state))
                 frontier.append(neighbor[index])
+                test_Frontier.add(frontier_State)
             begin = 0
   
         else:
-            test_Frontier = set()
-            for index in range(len(frontier)):
-                test_frontier_State = ''.join(map(str, frontier[index].state))
-                test_Frontier.add(test_frontier_State)
-
+            
             for index in range(len(neighbor)):
                 neighbor_State = ''.join(map(str, neighbor[index].state))
                 
@@ -223,8 +244,77 @@ def dfs(initial_State, goal_Test):
                     frontier.append(neighbor[index])
       
 
-# def ast(initial_State, goal_Test):
+def ast(initial_State, goal_Test):
+    start = time.time()
+    frontier = PriorityQueue()
+    explored = set()
+    test_Frontier = set()
+    begin = 1
+    nodes_expanded = 0
+    max_depth = 0
+    init_Node = create_Node(initial_State, None, 0, 0, 0)
+    frontier.put(init_Node)
+    max_fringe_size = 0
 
+    while frontier:
+ 
+        if len(frontier) > max_fringe_size:
+            max_fringe_size = len(frontier)
+        
+         	
+        state = frontier.pop()
+        test_State = list(map(int, state.state))
+        explored_State = ''.join(map(str, state.state))
+        explored.add(explored_State)
+
+        if not begin:
+            test_Frontier.remove(explored_State)
+ 
+        if max_depth < state.depth:
+            max_depth = state.depth
+
+        if test_State == goal_Test:
+            stop = time.time()
+            running_time = stop - start
+            fringe_size = len(frontier)
+            path = []
+            cost = 0
+            depth = 0
+            nodes_expanded = len(explored) - 1
+            last_node = frontier.pop()
+            
+            while state and state.parent != None:
+                path.insert(0, state.direction)
+                cost += state.cost
+                depth += 1
+                state = state.parent
+               
+
+            ram = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1000
+            
+            output_file(path, cost, nodes_expanded, fringe_size, max_fringe_size, depth, max_depth, running_time, ram)
+
+
+            return
+
+        neighbor = create_Frontier(state)
+        neighbor.reverse()  
+        
+        if not frontier and begin == 1:
+            for index in range(len(neighbor)):
+                frontier_State = ''.join(map(str, neighbor[index].state))    
+                frontier.append(neighbor[index])
+                test_Frontier.add(frontier_State)
+            begin = 0
+  
+        else:
+            
+            for index in range(len(neighbor)):
+                neighbor_State = ''.join(map(str, neighbor[index].state))
+                
+                if neighbor_State not in test_Frontier and neighbor_State not in explored:
+                    test_Frontier.add(neighbor_State)
+                    frontier.append(neighbor[index])
 
 
 
